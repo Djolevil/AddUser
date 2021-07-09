e5eabec6759fd325dc77c4a448b4a55923ec94a3
Ђорђе <37450617+Djolevil@users.noreply.github.com>
2021-07-09 21:07:13 +0200

    Add CSS styling for AddUser component via CSS modules

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,12 @@
 import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 const AddUser = props => {
     const addUserHandler = (event) => {
         event.preventDefault();
     };
     return (
-      <Card>
+      <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor='username'>Username</label>
           <input id='username' type='text' />
